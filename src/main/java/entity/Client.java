d67984858c7b79a3482c3ac5429a31cc4fda80c4
OSrB2 <pedro_oliva@outlook.com.br>
2025-02-11 09:35:36 -0300

    refact Client entity

--- src/main/java/entity/Client.java
+++ src/main/java/entity/Client.java
@@ -1,17 +1,15 @@
 package entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
+@Table(name = "tb_client")
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
-public class ClientEntity {
+public class Client {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
